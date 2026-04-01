53cf1786d45b906eb67c6260cf291ce95aa58a08
Corey Michaud <cor3y.michaud@gmail.com>
2026-04-01 16:45:25 -0400

    Added comment for clarity

--- filter/filters.py
+++ filter/filters.py
@@ -2,6 +2,10 @@
 # This script defines the filters that are applied to the scraper.
 # =================================================================
 
+# The `allow` list contains terms that you want to include in any job titles found.
+# The `deny` list contains terms that you want to exclude from any job titles found.
+# Try all combinations of terms to be safe, e.g. "jr" and "jr." for "junior", "sr" and "sr." for "senior", etc.
+
 import re
 
 allow = [
