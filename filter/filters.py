b65b95f9f408de5ebbd36561b9bd7e9c93e26ded
Corey Michaud <cor3y.michaud@gmail.com>
2026-04-09 13:40:38 -0400

    Changed allow/deny lists to use the list from the config

--- filter/filters.py
+++ filter/filters.py
@@ -5,59 +5,16 @@
 # Try all combinations of terms to be safe, e.g. "jr" and "jr." for "junior", "sr" and "sr." for "senior", etc.
 
 import re
+import yaml
 
-allow = [
-    "junior",
-    "jr",
-    "jr.",
-    "data",
-    "analyst",
-    "analytics",
-    "junior",
-    "entry",
-    "scientist",
-    "ai",
-    "machine learning",
-    "ml",
-    "ml/ai",
-    "statistics",
-    "statistician",
-    "associate",
-    "bi",
-    "business intelligence",
-    "reporting",
-]
-
-deny = [
-    "senior",
-    "sr.",
-    "sr",
-    "principal",
-    "director",
-    "president",
-    "vice president",
-    "vp",
-    "manager",
-    "staff",
-    "researcher",
-    "lead",
-    "research",
-    "director",
-    "executive",
-    "head",
-    "software",
-    "intern",
-    "summer",
-    "fall",
-    "winter",
-    "chief",
-    "officer",
-    "trainer",
-]
+with open("config/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 
 
 def filter_title(title: str) -> bool:
     title = title.lower()
+    allow = config["filters"]["allow"]
+    deny = config["filters"]["deny"]
 
     allowed = any(re.search(rf"\b{re.escape(term)}\b", title) for term in allow)
     denied = any(re.search(rf"\b{re.escape(term)}\b", title) for term in deny)
